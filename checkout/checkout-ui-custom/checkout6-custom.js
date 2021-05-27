975c0f073f9be0a7498afb21fa838ea7f2f6d1ee
Tomas Amoretti <tom.ia@hotmail.com>
2021-05-27 15:48:10 -0300

    cambios finales

---
+++ checkout/checkout-ui-custom/checkout6-custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $.ajaxSetup({
+    complete: function() {
+      $(".checkout-container .cart-template.active .cart-select-gift-placeholder").insertAfter($( ".table.table.cart-items" ));
+    }
+  });
+  function quitarDecimales(precio) {
+      return precio.split(",").shift();
+  }
+  if(window.location.href.includes('cart')) {
+    if(document.querySelectorAll(".item-link-remove") != null){
+      let remove = document.querySelectorAll(".item-link-remove");
+      remove.forEach((itemremove) => {
+          itemremove.children[0].className = "icon icon-trash item-remove-ico";
+      });
+    }
+  }
+
+  var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = '/arquivos/favicon.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
+
+});
