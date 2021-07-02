c25149c6fe25a3571a9d953a1a4fd093e1049dea
Julian Studnik <julian.s@knownonline.com>
2021-07-02 12:04:10 -0300

    agrego script custom klaviyo pixel checkout

--- checkout/checkout-ui-custom/checkout6-custom.js
+++ checkout/checkout-ui-custom/checkout6-custom.js
@@ -186,6 +186,60 @@ $(document).ready(function(){
     }
   }, 2000);
 
+  let klaviyoNotified = false
+  var e = document.createElement('script')
+  ;(e.type = 'text/javascript'),
+    (e.async = true),
+    (e.src = 'https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=ABCDEF')
+  var t = document.getElementsByTagName('script')[0]
+  t.parentNode.insertBefore(e, t)
+  $(window).on('hashchange', function() {
+    const { orderForm } = vtexjs.checkout
+    if (orderForm.clientProfileData && !klaviyoNotified && _learnq) {
+      _learnq.push([
+        'identify',
+        {
+          $email: orderForm.clientProfileData.email,
+          $first_name: orderForm.clientProfileData.firstName,
+          $last_name: orderForm.clientProfileData.lastName,
+        },
+      ])
+      let klaviyoItems = []
+      let productNames = []
+      let categories = []
+      orderForm.items.forEach(item => {
+        productNames.push(item.name)
+        const key = Object.keys(item.productCategories)[0]
+        categories.push(item.productCategories[key])
+        klaviyoItems.push({
+          ProductID: item.productRefId,
+          SKU: item.id,
+          ProductName: item.name,
+          Quantity: item.quantity,
+          ItemPrice: item.sellingPrice / 100,
+          RowTotal: item.price / 100,
+          ProductURL: window.location.hostname + item.detailUrl,
+          ImageURL: item.imageUrl,
+          ProductCategories: Object.values(item.productCategories),
+        })
+      })
+      _learnq.push([
+        'track',
+        'Started Checkout',
+        {
+          $event_id: orderForm.orderFormId,
+          $value: orderForm.value / 100,
+          ItemNames: productNames,
+          CheckoutURL: window.location.hostname + '/checkout#/cart',
+          Categories: categories,
+          Items: klaviyoItems,
+        },
+      ])
+      klaviyoNotified = true
+    }
+  })
+
+
 });
 
 window.onhashchange = function () {
